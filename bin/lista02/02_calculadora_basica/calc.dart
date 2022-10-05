0844cba704ecb9cdc0401626e250e8149e5f65f0
edaccp <edaccp@gmail.com>
2022-10-05 00:40:02 -0300

    renamed:    bin/01_area_quadrado.dart -> bin/lista01/01_area_quadrado.dart 	renamed:    bin/02_soma_produtos.dart -> bin/lista01/02_soma_produtos.dart 	renamed:    bin/03_calcular_desconto.dart -> bin/lista01/03_calcular_desconto.dart 	renamed:    bin/04_percentual_desconto.dart -> bin/lista01/04_percentual_desconto.dart 	renamed:    bin/05_inverter_valores.dart -> bin/lista01/05_inverter_valores.dart 	renamed:    bin/06_saldo_devedor.dart -> bin/lista01/06_saldo_devedor.dart 	renamed:    bin/07_calcular_media.dart -> bin/lista01/07_calcular_media.dart 	renamed:    bin/08_aprovado_reprovado.dart -> bin/lista01/08_aprovado_reprovado.dart 	renamed:    bin/09_string_vazia.dart -> bin/lista01/09_string_vazia.dart 	renamed:    bin/10_valor_nulo.dart -> bin/lista01/10_valor_nulo.dart 	renamed:    bin/11_par_impar_zero.dart -> bin/lista01/11_par_impar_zero.dart 	renamed:    bin/12_maior_menor.dart -> bin/lista01/12_maior_menor.dart 	renamed:    bin/13_peso_ideal.dart -> bin/lista01/13_peso_ideal.dart 	renamed:    bin/14_menu_crud.dart -> bin/lista01/14_menu_crud.dart 	renamed:    bin/15_desafio_ano_bissexto.dart -> bin/lista01/15_desafio_ano_bissexto.dart 	renamed:    bin/16_ate_valor_informado.dart -> bin/lista01/16_ate_valor_informado.dart 	renamed:    bin/17_numeros_100_300.dart -> bin/lista01/17_numeros_100_300.dart 	renamed:    bin/18_pares_menores_50.dart -> bin/lista01/18_pares_menores_50.dart 	renamed:    bin/19_dobro_do_numero/dobro.dart -> bin/lista01/19_dobro_do_numero/dobro.dart 	renamed:    bin/20_impares_valor_informado/impares.dart -> bin/lista01/20_impares_valor_informado/impares.dart 	renamed:    bin/21_quadrado_impares/quadrado.dart -> bin/lista01/21_quadrado_impares/quadrado.dart 	renamed:    bin/22_tamanho_palavra/tamanho.dart -> bin/lista01/22_tamanho_palavra/tamanho.dart 	renamed:    bin/23_desafio_estatisticas_equipe/estatisticas.dart -> bin/lista01/23_desafio_estatisticas_equipe/estatisticas.dart 	new file:   bin/lista02/02_calculadora_basica/calc.dart 	new file:   bin/lista02/03_validar_idade/idade.dart 	new file:   bin/lista02/04_calculo_recursivo/README.md 	new file:   bin/lista02/05_sequencia_fibonacci/README.md 	renamed:    bin/readmeGitHub.md -> bin/z_readmeGitHub.md 	deleted:    lista01/README.md 	deleted:    lista01/exercises/bin/01_area_quadrado/README.md 	deleted:    lista01/exercises/bin/02_soma_produtos/README.md 	deleted:    lista01/exercises/bin/03_calcular_desconto/README.md 	deleted:    lista01/exercises/bin/04_percentual_desconto/README.md 	deleted:    lista01/exercises/bin/05_inverter_valores/README.md 	deleted:    lista01/exercises/bin/06_saldo_devedor/README.md 	deleted:    lista01/exercises/bin/07_calcular_media/README.md 	deleted:    lista01/exercises/bin/08_aprovado_reprovado/README.md 	deleted:    lista01/exercises/bin/09_string_vazia/README.md 	deleted:    lista01/exercises/bin/10_valor_nulo/README.md 	deleted:    lista01/exercises/bin/11_par_impar_zero/README.md 	deleted:    lista01/exercises/bin/12_maior_menor/README.md 	deleted:    lista01/exercises/bin/13_peso_ideal/README.md 	deleted:    lista01/exercises/bin/14_menu_crud/README.md 	deleted:    lista01/exercises/bin/15_desafio_ano_bissexto/README.md 	deleted:    lista01/exercises/bin/16_ate_valor_informado/README.md 	deleted:    lista01/exercises/bin/17_numeros_100_300/README.md 	deleted:    lista01/exercises/bin/18_pares_menores_50/README.md 	deleted:    lista02/flutter-m01-a07-t02

---
+++ bin/lista02/02_calculadora_basica/calc.dart
@@ -0,0 +1,105 @@
+﻿// ![](https://i.imgur.com/xG74tOh.png)
+
+// # M01 - A07 - Exercício 2 - Calculadora Básica
+
+// ## Objetivo
+
+// Calcular os valores informados conforme a operação escolhida.
+
+// ## Instruções
+
+// 1. Escreva um algoritmo em Dart que receba 2** valores. ** São 3 valores com a operação
+// 2. Crie 4 functions para calcular os valores informados conforme a operação escolhida
+// (adição, subtração, divisão, multiplicação).
+// 3. Crie uma function para mostrar o resultado na tela.
+
+// ## Casos de Testes
+
+// ```
+// Entrada:
+// 	valor1 = 5.3
+// 	valor2 = 4.25
+// 	operação: adição
+
+// Saída:
+// 	Resultado de 5.3 + 4.25: 9.55
+// ```
+
+// ```
+// Entrada:
+// 	valor1 = 5.3
+// 	valor2 = 4.25
+// 	operação: multiplicação
+
+// Saída:
+// 	Resultado de 5.3 * 4.25: 22.52
+// ```
+
+// ```
+// Entrada:
+// 	valor1 = 5.3
+// 	valor2 = 4.25
+// 	operação: 5
+
+// Saída:
+// 	Operação inválida
+// ```
+
+// ### [Vídeo explicativo](https://drive.google.com/file/d/1q1-5yf0lvURQcFNU8uI3l6fr6WBI5feo/view?usp=sharing)
+
+void main(List<String> args) {
+// Entrada:
+  var valor1 = 5.3;
+  var valor2 = 4.25;
+  var oper = 'subtração';
+
+  add(a, b) {
+    return (a + b);
+  }
+
+  sub(a, b) {
+    return (a - b);
+  }
+
+  mult(a, b) {
+    return (a * b);
+  }
+
+  div(a, b) {
+    return (a / b);
+  }
+
+  void saida(a, b, c) {
+    switch (c) {
+      case 'adição':
+        {
+          print('Resultado de $a + $b: ${add(a, b).toStringAsFixed(2)}');
+        }
+        break;
+
+      case 'subtração':
+        {
+          print('Resultado de $a - $b: ${sub(a, b).toStringAsFixed(2)}');
+        }
+        break;
+      case 'multiplicação':
+        {
+          print('Resultado de $a * $b: ${mult(a, b).toStringAsFixed(2)}');
+        }
+        break;
+      case 'divisão':
+        {
+          print('Resultado de $a / $b: ${div(a, b).toStringAsFixed(2)}');
+        }
+        break;
+
+      default:
+        {
+          print('Operação Inválida');
+        }
+        break;
+    }
+  }
+
+  saida(valor1, valor2, oper);
+}
---
+++ bin/lista02/03_validar_idade/idade.dart
@@ -0,0 +1,72 @@
+﻿// ![](https://i.imgur.com/xG74tOh.png)
+// # M01 - A07 - Exercício 3 - Validar Idade
+// ## Objetivo
+// Verificar se a pessoa pode dirigir.
+// ## Instruções
+// 1. Escreva um algoritmo em Dart que receba o nome e a data de nascimento de uma pessoa (dia, mês e ano).
+// 2. Crie uma function para calcular a idade atual.
+// 3. Se a idade for maior ou igual a 18 anos, mostre "Pode dirigir!".
+// 4. Senão, mostre "Não pode dirigir".
+// ## Casos de Testes
+// ```
+// Entrada:
+// 	nome: Cristiane
+// 	dia: 2
+// 	mês: 10
+// 	ano: 1998
+// Saída:
+// 	Nome: Cristiane
+// 	Idade: 23
+// 	Pode dirigir!
+// ```
+// ```
+// Entrada:
+// 	nome: Gabriel
+// 	dia: 30
+// 	mês: 12
+// 	ano: 2018
+// Saída:
+// 	Nome: Gabriel
+// 	Idade: 3
+// 	Não pode dirigir
+// ```
+// ```
+// Entrada:
+// 	nome: Miguel
+// 	dia: 1
+// 	mês: 2
+// 	ano: 2003
+// Saída:
+// 	Nome: Miguel
+// 	Idade: 18
+// 	Pode dirigir!
+// ```
+// ### [Vídeo explicativo](https://drive.google.com/file/d/1mjtrBEx5g-PVMY2HBP4bY-lYGr_CCSfi/view?usp=sharing)
+void main(List<String> args) {
+// Entrada:
+  var nome = 'Miguel';
+  var dia = 5;
+  var mes = 10;
+  var ano = 2004;
+
+  idade(d, m, a) {
+    final hoje = DateTime.now();
+    var data = DateTime(ano, mes, dia);
+    var anos = hoje.difference(data);
+
+    // print('${(idade.inDays / 365).toStringAsFixed(1)} anos');
+
+    return ((anos.inDays - 4) / 365);
+  }
+
+  var vida = double.parse(idade(dia, mes, ano).toStringAsFixed(8));
+
+// 	Nome: Miguel
+  print('Nome: $nome $vida');
+// 	Idade: 18
+  print('Idade: ${vida ~/ 1} anos');
+// 	Pode dirigir!
+  if (vida >= 18) {
+    print('Pode dirigir');
+  }
+}
